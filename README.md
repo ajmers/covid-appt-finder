60c6709006a44779734c1306e1626587eb1bc353
Anne Maiale Mead <anne@stitchdata.com>
2021-02-04 16:27:00 -0500

    add readme

---
+++ README.md
@@ -0,0 +1,19 @@
+This program checks a Calendly calendar for appointments and sends a text message when one is found.
+
+## Prerequisites
+In order to get a text message when an appointment is found, this program utilizes Twilio. You will need to
+set up a Twilio API Key/Secret combination. You will start your free trial with around $15 in credit which is enough to send 
+a few hundred text messages.
+https://www.twilio.com/console
+
+Once you have set up your Twilio API Key, copy the SID and Secret into a file called `.env`:
+CLIENT_ID=<SID>
+CLIENT_SECRET=<SECRET>
+
+If you would like to choose a different Calendly calendar than the ones in `main.py`, you will need to change the properties of the `calendar` map and find the appropriate base URL. (You can check the network tab to see what API calls the calendly calendar is making, and strip off the query string parameters because we'll add our own). 
+
+Once the calendar is the one you want, run
+`python main.py`
+
+## Other customizable parameters:
+- How long should the program sleep before making the next request
--- main.py
+++ main.py
@@ -10,26 +10,24 @@ CLIENT_ID = os.getenv("CLIENT_ID")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 
 client = Client(CLIENT_ID, CLIENT_SECRET)
-covid_calendar = {
+calendar = {
     "name": "Eric's RX Covid Vaccines",
-    "base_url": "https://calendly.com/api/booking/event_types/EBAXZXNO5XL6LLRJ/calendar/range?timezone=America%2FNew_York&diagnostics=false"}
-
-lawyers_calendar = {
-    "name": "Lawyers",
-    "url": "https://calendly.com/api/booking/event_types/DDEEEBW4KOBI57MM/calendar/range?timezone=America%2FNew_York&diagnostics=false&range_start=2021-02-01&range_end=2021-02-28"}
+    "base_url": "https://calendly.com/api/booking/event_types/EBAXZXNO5XL6LLRJ/calendar/range?timezone=America%2FNew_York&diagnostics=false",
+    "booking_url": "https://calendly.com/eric-s-covid-vaccine/covid-vaccine"}
 
 def send_success_sms(calendar, start_time):
 	# change the "from_" number to your Twilio number and the "to" number
 	# to the phone number you signed up for Twilio with, or upgrade your
 	# account to send SMS to any phone number
     calendar_name = calendar['name']
+    booking_url = calendar['booking_url']
     client.messages.create(to="+14842011537",
                            from_="+16178632712",
-                           body= f'Found an appointment for {calendar_name}!! Appointment time: {start_time}')
+                           body= f'Found an appointment for {calendar_name}!! Appointment time: {start_time}. Click here to book: {booking_url}')
 
 def format_date_range():
     start = datetime.now()
-    end = start + timedelta(days = 31)
+    end = start + timedelta(days = 60)
     
     date_format = "%Y-%m-%d"
     formatted_start = datetime.strftime(start, date_format)
@@ -37,10 +35,8 @@ def format_date_range():
     return f'&range_start={formatted_start}&range_end={formatted_end}'
     
 def check_next_four_weeks():
-    calendar = covid_calendar
-
     request_url = calendar['base_url'] + format_date_range()
-    print("Request URL:", request_url)
+    #print("Request URL:", request_url)
     resp = requests.get(request_url)
     resp_obj = resp.json()
     days = resp_obj['days']
@@ -55,10 +51,10 @@ def check_next_four_weeks():
             send_success_sms(calendar, first_time_formatted)
             return True
         except StopIteration:
-            print("No slots found with availability")
+            pass
 
     except StopIteration:
-        print("No days found with availability")
+        pass
 
     return False
 
@@ -67,13 +63,13 @@ def keep_checking():
 
     if result == True:
         # found appointment, wait a longer time to check again
-        print(f'{datetime.now().strftime("%H:%M:%S")}: Found an appointment. Will wait 30 minutes to check again.')
-        time.sleep(60 * 30)
+        print(f'{datetime.now().strftime("%H:%M:%S")}: Found an appointment. Will wait 20 seconds to check again.')
+        time.sleep(20)
         keep_checking()
     else:
         # no appointment found, keep checking every minute:
-        print(f'{datetime.now().strftime("%H:%M:%S")}: No appointment found, checking again in a minute.')
-        time.sleep(60)
+        print(f'{datetime.now().strftime("%H:%M:%S")}: No appointment found, checking again in 20 seconds.')
+        time.sleep(20)
         keep_checking()
 
 keep_checking()
